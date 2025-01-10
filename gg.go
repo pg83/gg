5c560765a83207caf9e69edb96ed102577dafef1
Anton Samokhvalov <anton@samokhvalov.xyz>
2025-01-10 23:33:16 +0300

    init

---
+++ gg.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+const (
+	ESC = "\x1b"
+	RST = ESC + "[0m"
+	R   = ESC + "[91m"
+	G   = ESC + "[92m"
+	Y   = ESC + "[93m"
+	B   = ESC + "[94m"
+	M   = ESC + "[95m"
+)
+
+func color(color string, s string) string {
+	return color + s + RST
+}
+
+type Exception struct {
+	what func() error
+}
+
+func (self *Exception) throw() {
+	panic(self)
+}
+
+func (self *Exception) catch(cb func(*Exception)) {
+	if self != nil {
+		cb(self)
+	}
+}
+
+func (self *Exception) fatal(code int, prefix string) {
+	fmt.Fprintf(os.Stderr, "%s%s: %v%s\n", R, prefix, self.what(), RST)
+	os.Exit(code)
+}
+
+func newException(e error) *Exception {
+	return &Exception{
+		what: func() error {
+			return e
+		},
+	}
+}
+
+func fmtException(format string, args ...any) *Exception {
+	return newException(fmt.Errorf(format, args...))
+}
+
+func try(cb func()) (err *Exception) {
+	defer func() {
+		if rec := recover(); rec != nil {
+			if exc, ok := rec.(*Exception); ok {
+				err = exc
+			} else {
+				// personality check failed
+				panic(rec)
+			}
+		}
+	}()
+
+	cb()
+
+	return nil
+}
+
+func main() {
+	try(func() {
+		
+	}).catch(func(exc *Exception) {
+		exc.fatal(1, "abort")
+	})
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module pg83/gg
+
+go 1.23.4
